d1ad36e795e338a6cd2938baaacd8bda7e274966
rahulsd380 <rahulsd380@gmail.com>
2025-06-25 00:11:14 +0600

    Completed solutions section

--- src/components/HomePage/SolutionsWeOffer/SolutionsWeOffer.tsx
+++ src/components/HomePage/SolutionsWeOffer/SolutionsWeOffer.tsx
@@ -1,6 +1,34 @@
-import { ICONS } from "../../../assets";
+import { ICONS, IMAGES } from "../../../assets";
 
 const SolutionsWeOffer = () => {
+  const solutions = [
+    {
+      image: IMAGES.marketing,
+      icon: ICONS.marketingIcon,
+      title: "Marketing",
+    },
+    {
+      image: IMAGES.development,
+      icon: ICONS.developmentIcon,
+      title: "Development",
+    },
+    {
+      image: IMAGES.contentProduction,
+      icon: ICONS.contentProductionIcon,
+      title: "Content Production",
+    },
+    {
+      image: IMAGES.aiAutomation,
+      icon: ICONS.aiAutomationIcon,
+      title: "AI Automation",
+    },
+    {
+      image: IMAGES.contentCreation,
+      icon: ICONS.contentCreationIcon,
+      title: "Content Creation",
+    },
+  ];
+
   return (
     <div className="font-Lato mt-[100px]">
       <h1 className="text-[50px] text-neutral-10 leading-[40px] font-bold text-center">
@@ -10,16 +38,26 @@ const SolutionsWeOffer = () => {
         Content Creation Services We Offer Tailored to your Goals
       </p>
 
-      <div className="flex gap-[30px] mt-[42px]">
-        <div className="bg-[#FDF8EE] hover:bg-gradient-to-b hover:from-[#FDEBE7]/50 hover:to-[#F5C7C0] 
-            transition-all duration-300 ease-in-out border border-[#7A7A7A] rounded-[10px] w-[350px] h-[476px]">
-          <div className="pt-5 px-[30px]">
-            <img src={ICONS.fire} alt="" className="size-[74px]" />
-            <h1 className="text-[40px] text-neutral-10 leading-[22px] font-semibold mt-5">
-              Marketing
-            </h1>
+      <div
+        className="flex gap-[30px] mt-[42px] overflow-x-auto snap-x snap-mandatory scroll-smooth px-4 pb-4
+        scrollbar-hide"
+      >
+        {solutions.map((data, index) => (
+          <div
+            key={index}
+            className="bg-[#FDF8EE] hover:bg-gradient-to-b hover:from-[#FDEBE7]/50 hover:to-[#F5C7C0]
+              transition-all duration-300 ease-in-out border border-[#7A7A7A] rounded-[10px]
+              w-[350px] h-[476px] flex-shrink-0 flex flex-col justify-between snap-start"
+          >
+            <div className="pt-4 px-[15px]">
+              <img src={data.icon} alt="" className="size-[74px]" />
+              <h1 className="text-[35px] text-neutral-10 leading-[22px] font-semibold mt-5">
+                {data.title}
+              </h1>
+            </div>
+            <img src={data.image} alt="" />
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
