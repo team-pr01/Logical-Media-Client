66c6484171352a17de13560c2dfb71275e0687db
prernabadwane <prernabadwane@gmail.com>
2025-06-25 00:04:44 +0530

    get in touch componenct added

--- src/components/reusable/GetInTouch/GetInTouch.tsx
+++ src/components/reusable/GetInTouch/GetInTouch.tsx
@@ -4,29 +4,47 @@ import { ICONS } from "../../../assets";
 
 const GetInTouch = () => {
   return (
-    <div className=" px-6 pt-6 pb-10">
-      <Container>
-        <div className="flex flex-col items-center justify-center h-full font-Lato">
-          <h1 className="text-[50px] text-[#FDF8EE] leading-[68px] font-semibold text-center mb-[52px]">
-            Get Your Business’s Free Audit Done Right Away
-          </h1>
+    <Container>
+      <div className="flex flex-col items-center justify-center h-full font-Lato py-[100px]">
+        <h1 className="text-[50px] text-black leading-[58px] font-semibold text-center mb-[52px]">
+          Want to Scale your Business with Our Content Creation Services?
+        </h1>
 
-          
-            <Link
-              to="/book-a-call"
-              className="bg-[#FDF8EE] border border-[#FDF8EE] hover:bg-transparent hover:text-[#FDF8EE] text-black text-[24px] px-10 py-2 rounded-[36px] shadow transition duration-300 "
-            >
-              Get Started
-              <img
-                                src={ICONS.fire}
-                                alt=""
-                                className="size-[50px] mr-2 mb-2"
-                              />
-            </Link>
-          
+        <Link
+          to="/book-a-call"
+          className="bg-primary-10 border border-primary-10 hover:bg-transparent hover:text-primary-10 text-[#FDF8EE] text-[24px] px-[14px] py-2 rounded-full flex transition duration-300 justify-center items-center gap-2 "
+        >
+          Get In Touch
+          <img src={ICONS.fire} alt="" className="size-[50px] " />
+        </Link>
+        <div className="grid grid-cols-3 items-center justify-center mt-[40px] w-full ">
+            <div className="flex items-center justify-center gap-2">
+                <img
+                  src={ICONS.fire}
+                  alt=""
+                  className="size-[30px] mr-2 mb-2"
+                />
+                <h1 className="text-[24px] text-black leading-[30px] font-medium">kjdjnio</h1>
+            </div>
+            <div className="flex items-center justify-center gap-2">
+                <img
+                  src={ICONS.fire}
+                  alt=""
+                  className="size-[30px] mr-2 mb-2"
+                />
+                <h1 className="text-[24px] text-black leading-[30px] font-medium">kjdjnio</h1>
+            </div>
+             <div className="flex items-center justify-center gap-2">
+                <img
+                  src={ICONS.fire}
+                  alt=""
+                  className="size-[30px] mr-2 mb-2"
+                />
+                <h1 className="text-[24px] text-black leading-[30px] font-medium">kjdjnio</h1>
+            </div>
         </div>
-      </Container>
-    </div>
+      </div>
+    </Container>
   );
 };
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,6 +4,7 @@ import OurApproach from "../../components/HomePage/OurApproach/OurApproach";
 import SolutionsWeOffer from "../../components/HomePage/SolutionsWeOffer/SolutionsWeOffer";
 import Testimonials from "../../components/HomePage/Testimonials/Testimonials";
 import TrustedBy from "../../components/HomePage/TrustedBy/TrustedBy";
+import GetInTouch from "../../components/reusable/GetInTouch/GetInTouch";
 import GetStartedCTA from "../../components/reusable/GetStartedCTA.tsx/GetStartedCTA";
 
 const Home = () => {
@@ -16,6 +17,7 @@ const Home = () => {
             <MakeUsDifferent/>
             <OurApproach/> 
             <Testimonials/>
+            <GetInTouch/>
         </div>
     );
 };
